85b76154adcbc36919c2c550622002e7c0a21d55
Joe S <kenwood364@gmail.com>
2019-07-11 17:49:01 -0400

    add ability to generate encoder numbers

--- python/fakeEncoder.py
+++ python/fakeEncoder.py
@@ -1,8 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import time
+from math import sin, cos
 from networktables import NetworkTables
-
 import logging	# Required
 
-# Needs to publish fake encoder values
+logging.basicConfig(level=logging.debug)
+
+NetworkTables.initialize()
+table = NetworkTables.getTable("SmartDashboard")
+
+fakePortEncoder = 0
+fakeStarboardEncoder = 0
+speedModifier = 100
+while true:
+    table.putNumber("port", fakePortEncoder)
+    table.putNumber("Starboard", fakeStarboardEncoder)
+
+    fakePortEncoder = speedModifier * (fakePortEncoder + (sin(fakePortEncoder) + sin(3 * fakePortEncoder) + sin(9 * fakePortEncoder)))
+    fakeStarboardEncoder = speedModifier * (fakeStarboardEncoder + (sin(fakeStarboardEncoder) + sin(3 * fakeStarboardEncoder) + sin(9 * fakeStarboardEncoder)))
+    time.sleep(0.01)
