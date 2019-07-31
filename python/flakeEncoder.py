56c315b1c10e7e84fcc00cedb2eb6d7dc058de1a
Joe S <kenwood364@gmail.com>
2019-07-31 16:35:31 -0400

    Create flakeEncoder.py

---
+++ python/flakeEncoder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import time
+from networktables import NetworkTables
+
+import logging	# Required
+logging.basicConfig(level=logging.DEBUG)
+
+NetworkTables.initialize()
+table = NetworkTables.getTable("SmartDashboard")
+
+fakePortEncoder = 0
+fakePortEncoder = 0
+speedModifier = 10
+
+while 1:
+    table.putNumber("Port", fakePortEncoder)
+    table.putNumber("Starboard", fakeStarboardEncoder)
+
+    print(fakePortEncoder)
+    print(fakeStarboardEncoder)
+    #print(i)
+
+    fakePortEncoder = fakePortEncoder + speedModifier
+    fakeStarboardEncoder = fakeStarboardEncoder + speedModifier
+    time.sleep(0.05)
