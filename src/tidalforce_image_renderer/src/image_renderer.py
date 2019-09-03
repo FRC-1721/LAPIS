f4be56b0f1bdbcb50a699f08ff9b16d0258e02c4
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2019-09-03 11:49:46 -0400

    Create rough outline for image_renderer.py

---
+++ src/tidalforce_image_renderer/src/image_renderer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import PointCloud2 as pc2
+from sensor_msgs.msg import LaserScan
+from laser_geometry import LaserProjection
+
+class laser_pointcloud():
+    def __init__(self):
+        self.laserProj = LaserProjection()
+        self.pcPub = rospy.Publisher ("/laserPointCloud", pc2, queue_size=1) # point cloud publisher
+        self.laserSub = rospy.Subscriber ("/scan", LaserScan, self.laserCallback) # subscribe to laser scan
+        print("init")
+
+    def laserCallback(self, data):
+        cloud_out = self.laserProj.projectLaser(data) # create cloud
+        self.pcPub.publish (cloud_out) # publish cloud out
+        print("scan")
+
+class pointcloud_image():
+    def __init__(self):
+        #more stuff
+
+if __name__ == '__main__':
+    rospy.init_node ("laserPC")
+    l2pc = laser_pointcloud()
+    rospy.spin()
