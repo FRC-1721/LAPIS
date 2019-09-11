1b07cad668882f0da7b55baa53a1c137d1a2331e
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2019-09-11 10:05:45 -0400

    Removed extra parenthsies
    Relates to comment on pull request #9
    Was unable to identify argparse use, will implement at a later date

--- src/tidalforce_odom/src/odom.py
+++ src/tidalforce_odom/src/odom.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     #ip = rospy.get_param("~ip", "roboRIO-1721-FRC")
     #ip = rospy.get_param("~ip", "localhost") # For testing
     #ip = rospy.get_param("~ip", "10.17.21.55") # First mDNS almost never works on the feild
-    if (len(sys.argv) > 1): # If there is more than one argument
+    if len(sys.argv) > 1: # If there is more than one argument
         ip = sys.argv[1] # Set the ip to the extra argument (remember to count from zero!)
     else: # if no extra argument
         ip = rospy.get_param("~ip", "10.17.21.55") # use the RIO ip by default
