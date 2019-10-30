c6e13b396df9384781ab0a5f7f161b34d0531f42
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2019-10-30 16:20:01 -0400

    It works!

--- robot_control/src/robot_control.py
+++ robot_control/src/robot_control.py
@@ -12,13 +12,14 @@ print ("Starting NetworkTables using IP: ", ip)
 NetworkTables.initialize(server = ip)
 table = NetworkTables.getTable("ROS")
 
-for i in range(10):
+for i in range(30):
     table.putNumber("coprocessorPort", 0.5)
-    table.putNumber("coprocessorPort", 0.5)
+    table.putNumber("coprocessorStarboard", -0.5)
     time.sleep(0.5)
     table.putNumber("coprocessorPort", -0.5)
-    table.putNumber("coprocessorPort", -0.5)
+    table.putNumber("coprocessorStarboard", 0.5)
     time.sleep(0.5)
     table.putNumber("coprocessorPort", 0)
-    table.putNumber("coprocessorPort", 0)
+    table.putNumber("coprocessorStarboard", 0)
     time.sleep(0.5)
+    print("Bruh: " + str(i))
