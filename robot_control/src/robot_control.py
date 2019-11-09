505ed53c457ea3a5df49ad1f2a16b69d0c7aabae
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2019-11-09 11:29:34 -0500

    Current design does not account for backwards driving

--- robot_control/src/robot_control.py
+++ robot_control/src/robot_control.py
@@ -15,15 +15,29 @@ table = NetworkTables.getTable("ROS")
 if __name__ == "__main__":
     rospy.init_node("robot_control")
     
-    turn_p = 0.1 # values for turning
-    turn_i = 0.1
-    turn_d = 0.1 
+    turn_p = 0.01 # values for turning
+    turn_i = 0.01
+    turn_d = 0.01 
     
-    drive_p = 0.1  # values for driving
-    drive_i = 0.1 
-    drive_d = 0.1 
+    drive_p = 0.01  # values for driving
+    drive_i = 0.01 
+    drive_d = 0.01 
     
     deadzone_angle = 90 # angle for deadzone
+    
+    def pid_drive(distance_error, instant_heading_error): # Instant heading is the desired heading at that moment along the spline
+        current_drive_i = drive_i + current_drive_i
+        current_turn_i = turn_i + current_turn_i
+        
+        thro = (distance_error * (dirve_p / drive_d)) + current_drive_i
+        thro = thro * deadzone_falloff(instant_heading_error) # if not facing the correct way dont drive forward
+        if thro > 1:
+            thro = 1
+            print ("Max Power!")
+        
+        table.putNumber("coprocessorPort", thro - steerage)
+        table.putNumber("coprocessorStarboard", (thro + steerage) * -1)
+
     def deadzone_falloff(error_angle): # falloff of deadzone (as the robot nears its target heading its speed will be multiplied
         error_angle = error_angle + 90
         #TODO map error angle between deadzone_angle
