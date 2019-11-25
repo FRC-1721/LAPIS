3348ac504891e094c6f438322e202a63f3acb754
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2019-11-25 07:49:32 -0500

    Created a tool that speaks alert messages from the robot

---
+++ tidalforce_odom/src/pynetworktables-tools/driver_station.py
@@ -0,0 +1,26 @@
+import pyttsx3
+import time
+from networktables import NetworkTables
+
+import logging # Required
+logging.basicConfig(level=logging.DEBUG)
+
+ip = "roboRIO-1721-FRC"
+hal9000 = pyttsx3.init()
+
+NetworkTables.initialize(server=ip)
+table = NetworkTables.getTable("SmartDashboard")
+
+hal9000.say("Robot Audio Service Started")
+hal9000.runAndWait()
+
+previous_alert = ""
+while True:
+    alert = table.getString("alert", "")
+    print(alert)
+    if alert != previous_alert:
+        hal9000.say(alert)
+        hal9000.runAndWait()
+    else:
+        time.sleep(1)
+    previous_alert = alert
