9d6b6c218a7534823b99eb69be30625ac2b7d69c
KhanSimeoni <simeonikh@gmail.com>
2020-01-22 18:21:17 -0500

    done

---
+++ hungry_toaster_gui/gui/InputSim.js
@@ -0,0 +1,6 @@
+var robotX = Math.round((Math.random()*821-(821/2))*0.316);
+var robotY = Math.round((Math.random()*1598-(1598/2))*0.316);
+robotX = String("calc(63vw + "+robotX+"px)");
+robotY = String("calc(52vh + "+robotY+"px)");
+
+var roboRotate = Math.round(Math.random()*360);
